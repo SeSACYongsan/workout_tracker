479e7f526b342d9f71d354df9bda7a78586abfae
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-08-28 13:44:10 +0900

    Update

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -6,8 +6,74 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow,
-      body: Center(
-        child: Image.asset("assets/images/runner.png"),
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/runner.png"),
+                  fit: BoxFit.cover,
+                  opacity: 0.2,
+                ),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 100),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white70,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "남들이 그만둘 때 난 계속한다",
+                        style:
+                            Theme.of(context).textTheme.headlineLarge!.copyWith(
+                                  fontFamily: "NanumBrushScript",
+                                ),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    textAlign: TextAlign.center,
+                    "환영합니다",
+                    style: Theme.of(context)
+                        .textTheme
+                        .displayMedium!
+                        .copyWith(fontFamily: "Jua"),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: Text(
+                        "START",
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
