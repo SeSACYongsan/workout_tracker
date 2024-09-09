d3ab66a368acb216c48080f1cd2626ae8e9dc027
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-09-09 09:52:32 +0900

    Update dashboard_card.dart

--- lib/components/dashboard_card.dart
+++ lib/components/dashboard_card.dart
@@ -8,9 +8,42 @@ class DashboardCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: Colors.grey.shade300,
+      ),
+      child: const Column(
+        children: [
+          Row(
+            children: [
+              Icon(
+                Icons.fitness_center,
+                size: 33,
+                color: Colors.orange,
+              ),
+              SizedBox(width: 10),
+              Text(
+                "Monthly",
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Center(
+              child: Text(
+                "12회",
+                style: TextStyle(
+                  fontSize: 33,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
