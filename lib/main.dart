d9a667e4c35278869faaecf17331f1fab8099f01
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-09-26 10:39:17 +0900

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:workout_tracker/firebase_options.dart';
 import 'package:workout_tracker/my_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MaterialApp.router(
       routerConfig: router,
