b4b7e1a66ac377b0501046ddd8704bcded927fd6
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-09-26 09:08:26 +0900

    Update landing_page.dart

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController animationController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,12 +18,15 @@ class LandingPage extends StatelessWidget {
       body: Stack(
         children: [
           Center(
-            child: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/runner.png"),
-                  fit: BoxFit.cover,
-                  opacity: 0.2,
+            child: FadeTransition(
+              opacity: animation,
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/runner.png"),
+                    fit: BoxFit.cover,
+                    opacity: 0.2,
+                  ),
                 ),
               ),
             ),
@@ -28,30 +39,38 @@ class LandingPage extends StatelessWidget {
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(20),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white70,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        textAlign: TextAlign.center,
-                        "남들이 그만둘 때 난 계속한다",
-                        style:
-                            Theme.of(context).textTheme.headlineLarge!.copyWith(
-                                  fontFamily: "NanumBrushScript",
-                                ),
+                    child: FadeTransition(
+                      opacity: animation,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white70,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          "남들이 그만둘 때 난 계속한다",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineLarge!
+                              .copyWith(
+                                fontFamily: "NanumBrushScript",
+                              ),
+                        ),
                       ),
                     ),
                   ),
                   const Spacer(),
-                  Text(
-                    textAlign: TextAlign.center,
-                    "환영합니다",
-                    style: Theme.of(context)
-                        .textTheme
-                        .displayMedium!
-                        .copyWith(fontFamily: "Jua"),
+                  FadeTransition(
+                    opacity: animation,
+                    child: Text(
+                      textAlign: TextAlign.center,
+                      "환영합니다",
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayMedium!
+                          .copyWith(fontFamily: "Jua"),
+                    ),
                   ),
                   const SizedBox(height: 20),
                   Padding(
@@ -79,5 +98,22 @@ class LandingPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    animation = Tween<double>(begin: 0, end: 1).animate(animationController);
+    animationController.forward();
+    super.initState();
   }
 }
